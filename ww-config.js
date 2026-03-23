18edfbee153aa28b5fa65c02b106c469a3c6bb42
MattS-98 <matthewsandys1@gmail.com>
2026-03-23 15:20:36 +0100

    update change trigger descriptions
    Refine generic change trigger copy to match each component behavior.

--- ww-config.js
+++ ww-config.js
@@ -8,7 +8,7 @@ export default {
     },
     states: ['drop'],
     triggerEvents: [
-        { name: 'change', label: 'On change', event: { value: DEFAULT_VALUE }, default: true, description: "Run when the value changes (when a file is uploaded)" },
+        { name: 'change', label: 'On change', event: { value: DEFAULT_VALUE }, default: true, description: "Run when a file is uploaded." },
         { name: 'initValueChange', label: 'On init value change', event: { value: DEFAULT_VALUE }, description: "Run when the initial value changes." },
         { name: 'invalidFile', label: 'On invalid file drop', event: { value: DEFAULT_VALUE }, description: "Run when an invalid file is dropped." },
     ],
